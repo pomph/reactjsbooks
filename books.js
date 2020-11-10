1516044caefb109af089c7d93fa93831eb189151
pomph <68281377+pomph@users.noreply.github.com>
2020-11-10 12:01:58 +0000

    Add files via upload

---
+++ books.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+     id: 1,
+     img:
+      'https://images-eu.ssl-images-amazon.com/images/I/81I5j2kswrL._AC_UL200_SR200,200_.jpg',
+     title: 'A Life On Our Planet',
+     author: 'David Attenborough',
+    },
+    {
+     id: 2,
+     img:
+      'https://images-eu.ssl-images-amazon.com/images/I/711grkRRyoL._AC_UL200_SR200,200_.jpg',
+     title: 'My Garden World',
+     author: 'Monty Don',
+    },
+    {
+     id: 3,
+     img:
+      'https://images-eu.ssl-images-amazon.com/images/I/71H8zbfC8WL._AC_UL200_SR200,200_.jpg',
+     title: 'Nala\'s World',
+     author: 'Dean Nicholson',
+    },
+   ];
